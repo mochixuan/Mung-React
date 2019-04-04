191bb7b5f9117516cccbd4aea5783568aeb697a1
wang <1306894924@qq.com>
2019-04-04 19:17:22 +0800

    config finish

---
+++ src/data/net/HttpBase.js
@@ -0,0 +1,90 @@
+const NETWORK_TIMEOUT = 15000; //超时时间15秒
+
+// 数据格式太乱,简单处理一下
+
+export const BaseGetRequest = (urlSuffix) => {
+    const opts = {method: 'GET'}
+    return timeoutPromise(fetch(urlSuffix,opts),NETWORK_TIMEOUT)
+        .then((result)=> errorAnalysis(result))
+        .catch((error)=> errorAnalysis(error))
+
+}
+
+const timeoutPromise = (request,timeoutMS) => {
+    return new Promise((resolve, reject)=>{
+        const requestTimeout = setTimeout(()=>{
+            reject(generateErrorData(TIMEOUT_ERROR_CODE))
+        },timeoutMS)
+
+        request.then(response => response.json())
+            .then((res)=>{
+                clearTimeout(requestTimeout)
+                res.code = CODE_SUCCESS //全局统一格式
+                resolve(res)
+            }).catch(()=>{
+                reject(generateErrorData(REQUEST_ERROR))
+            })
+    })
+}
+
+
+export const CODE_SUCCESS = 1   //自定义成功
+const REQUEST_ERROR = -100;    //网络请求错误
+const TIMEOUT_ERROR_CODE = -101 // 网络超时,请重试!
+const INVALID_API_KEY=104; //apikey无线
+const BAD_REQUEST = 400; //请求的地址不存在或者包含不支持的参数
+const UNAUTHORIZED = 401;  //数据未授权
+const FORBIDDEN = 403;  //数据被禁止访问访问
+const NOT_FOUND = 404;  //请求的资源不存在或被删除
+const INTERNAL_SERVER_ERROR = 500;  //内部错误
+const NEED_PERMISSION = 1000;  //数据未授权
+
+const generateErrorData = (code) => {
+    return {code: code}
+}
+
+const errorAnalysis = (result) => {
+
+    if(!result || result.code == undefined || result.code == null) {
+        return {
+            code: REQUEST_ERROR,
+            error: '网络超时,请重试!',
+            data: undefined
+        }
+    }
+
+    switch (result.code) {
+        case CODE_SUCCESS:
+            result.error = "请求成功"
+            break
+        case TIMEOUT_ERROR_CODE:
+            result.error = "网络超时,请重试!"
+            break
+        case REQUEST_ERROR:
+            result.error = "网络请求错误";
+            break;
+        case INVALID_API_KEY:
+            result.error = "ApiKey无效了";
+            break;
+        case BAD_REQUEST:
+            result.error = "请求的地址不存在或者包含不支持的参数";
+            break;
+        case UNAUTHORIZED:
+            result.error = "数据未授权";
+            break;
+        case FORBIDDEN:
+            result.error = "数据被禁止访问访问";
+            break;
+        case NOT_FOUND:
+            result.error = "请求的资源不存在或被删除";
+            break;
+        case INTERNAL_SERVER_ERROR:
+            result.error = "内部错误";
+            break;
+        case NEED_PERMISSION:
+            result.error = "数据未授权";
+            break;
+    }
+
+    return result
+}
--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -1,22 +1,74 @@
 import React from 'react'
 import styles from './mainpage.scss'
 import {inject,observer} from 'mobx-react'
+import ReactSwipe from 'react-swipe'
+//import {BaseGetRequest} from '../../data/net/HttpBase'
 
 @inject('themeStore')
 @observer
 class MainPage extends React.Component{
 
+    constructor(props) {
+        super(props)
+
+        //初始化swipeOpt
+        this.swipeOptions = {
+            auto: 3000,
+            callback: this.onSwipeCallBack,
+        }
+
+        this.state = {
+            text: 'asas'
+        }
+
+    }
+
+    componentDidMount() {
+        fetch("https://api.douban.com/v2/movie/in_theaters?start=1&count=10",{
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                }
+            })
+            .then((response)=>response.json())
+            .then((result)=>{
+                console.warn('result',result)
+                this.setState({text: 'success'})
+            }).catch((error)=>{
+                console.warn('error',error)
+                this.setState({text: 'error'})
+            })
+    }
+
     render() {
         const themeBgObj = {backgroundColor: this.props.themeStore.themeColor}
-
         return (
             <div className={styles.container}>
+
                 <div className={styles.header} style={themeBgObj}>
-                    <div></div>
+                    <i className={'iconfont base-small-circle-icon'}>&#60140;</i>
                     <div className={'base-title'}>Mung</div>
-                    <i className={'iconfont base-small-icon'}>&#60158;</i>
+                    <i className={'iconfont base-small-circle-icon'}>&#60158;</i>
                 </div>
+
+                <div className={styles.banner} style={themeBgObj}>
+                    <ReactSwipe swipeOptions={this.swipeOptions}>
+                        {this.renderSwipeItemView()}
+                    </ReactSwipe>
+                </div>
+
+                <div>{this.state.text}</div>
+
             </div>
+        )
+    }
+
+    onSwipeCallBack(swipeIndex) {
+
+    }
+
+    renderSwipeItemView() {
+        return (
+            <div>dadada</div>
         )
     }
 
